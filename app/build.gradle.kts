feca76f2b62d8e3f4b2d200c63163c1f8004ab42
Trist <duonghd1307@gmail.com>
2022-06-19 10:05:52 +0700

    Upgrade version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -121,9 +121,9 @@ dependencies {
     implementation(project(":modules:hd-core"))
     implementation(project(":modules:hd-auth"))
 
-    implementation("androidx.appcompat:appcompat:1.4.1")
+    implementation("androidx.appcompat:appcompat:1.4.2")
     implementation("androidx.activity:activity-ktx:1.4.0")
-    implementation("androidx.fragment:fragment-ktx:1.4.0")
+    implementation("androidx.fragment:fragment-ktx:1.4.1")
     implementation("androidx.viewpager2:viewpager2:1.0.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 }
